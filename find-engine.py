55b5dabc2c0f85ba5bb579c3c033b69d28aff1d8
Faalagorn <faalagorn@gmail.com>
2019-12-01 23:17:19 +0100

    Added LWJGL
    Added LWJGL. Might need some further testing and needs to be pointed at .jar currently

--- find-engine.py
+++ find-engine.py
@@ -91,6 +91,12 @@ def main():
 				print("Found Unreal Engine version: 4")
 				set_clip_v("Unreal Engine 4")
 
+			match = re.search(br'org\/lwjgl\/LWJGL', file_data)
+			if(not found and match is not None):
+				found = True
+				print("Found LWJGL")
+				set_clip_v("LWJGL")
+
 			match = re.search(br'\r\nKirikiri Z Project Contributors\r\nW.Dee, casper', file_data)
 			if(not found and match is not None):
 				found = True
