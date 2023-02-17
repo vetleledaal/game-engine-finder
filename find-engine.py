37b3030f4eac5336c65f14473e8d1cd3b6074d18
Splendide Imaginarius <119545140+Splendide-Imaginarius@users.noreply.github.com>
2023-02-17 04:24:07 +0000

    Factor detect() from main()
    Makes it easier to call game-engine-finder as a library.

--- find-engine.py
+++ find-engine.py
@@ -15,18 +15,16 @@ username = "Vetle" # PCGamingWiki username to be linked in refcheck
 # AGS - Adventure Game Studio (2.72 checked) -- ????
 # /ACI version (\d\.[\w\s\-]{1,8}(?:\.[\w\s\-]{1,8})?)[\n\)\[,]/
 
-def main():
-	set_clip("QUIT PREMATURELY");
+def detect(exe):
 	found = False
 	# Check dir struct
-	exe_path = os.path.dirname(sys.argv[1])
+	exe_path = os.path.dirname(exe)
 
 	path = os.path.join(exe_path, "Adobe AIR")
 	if(not found and os.path.exists(path)):
 		# Probably Adobe AIR (not tested)
 		print('Found Adobe AIR')
-		set_clip_v("Adobe AIR")
-		found = True
+		return "Adobe AIR", None
 
 	path = os.path.join(exe_path, "renpy")
 	if(not found and os.path.exists(path)):
@@ -51,14 +49,14 @@ def main():
 			found = True
 			ver = ".".join(map(str, match1[1::2] + match2[1::2]))
 			print("Found Ren'Py version: " + ver)
-			set_clip_v("Ren'Py", ver)
+			return "Ren'Py", ver
 		else:
 			print("Found Ren'Py, no ver")
 			print(match1 is not None)
 			print(match2 is not None)
 
 	if(not found):
-		with open(sys.argv[1], 'rb') as file:
+		with open(exe, 'rb') as file:
 			file_data = file.read()
 
 			match = re.search(br'\x00UnityPlayer\/([^\x20]+)', file_data)
@@ -79,68 +77,68 @@ def main():
 			if(not found and match is not None):
 				found = True
 				print("Found Unity version: " + match.group(1).decode('utf-8'))
-				set_clip_v("Unity", match.group(1).decode('utf-8'))
+				return "Unity", match.group(1).decode('utf-8')
 
 			match = re.search(br'Made with GameMaker: Studio\x00', file_data)
 			if(not found and match is not None):
 				found = True
 				print("Found GameMaker")
-				set_clip_v("GameMaker")
+				return "GameMaker", None
 			else: # Fallback, Windows only
 				match = re.search(br'name="YoYoGames.GameMaker.Runner"', file_data)
 				if(not found and match is not None):
 					found = True
 					print("Found GameMaker")
-					set_clip_v("GameMaker")
+					return "GameMaker", None
 
 			match = re.search(br'Unreal Engine 3 Licensee\x00{4}Unreal Engine 3', file_data)
 			if(not found and match is not None):
 				found = True
 				print("Found Unreal Engine version: 3")
-				set_clip_v("Unreal Engine 3")
+				return "Unreal Engine 3", None
 
 			# \UE4PrereqSetup (UNICODE)
 			match = re.search(br'\x5C\x00\x55\x00\x45\x00\x34\x00\x50\x00\x72\x00\x65\x00\x72\x00\x65\x00\x71\x00\x53\x00\x65\x00\x74\x00\x75\x00\x70\x00', file_data)
 			if(not found and match is not None):
 				found = True
 				print("Found Unreal Engine version: 4")
-				set_clip_v("Unreal Engine 4")
+				return "Unreal Engine 4", None
 
 			match = re.search(br'org\/lwjgl\/LWJGL', file_data)
 			if(not found and match is not None):
 				found = True
 				print("Found LWJGL")
-				set_clip_v("LWJGL")
+				return "LWJGL", None
 
 			match = re.search(br'com\/badlogic\/gdx', file_data)
 			if(not found and match is not None):
 				found = True
 				print("Found LibGDX")
-				set_clip_v("LibGDX")
+				return "LibGDX", None
 
 			match = re.search(br'\r\nKirikiri Z Project Contributors\r\nW.Dee, casper', file_data)
 			if(not found and match is not None):
 				found = True
 				print("Found KiriKiri Z")
-				set_clip_v("KiriKiri Z")
+				return "KiriKiri Z", None
 
 			match = re.search(br'\x00__ZL17mkxpDataDirectoryiPmm\x00', file_data)
 			if(not found and match is not None):
 				found = True
 				print("Found mkxp")
-				set_clip_v("mkxp")
+				return "mkxp", None
 
 			match = re.search(br'\x00Software\\(KADOKAWA|Enterbrain)\\RPG2000\x00', file_data)
 			if(not found and match is not None):
 				found = True
 				print("Found RPG Maker version: 2000")
-				set_clip_v("RPG Maker", "2000")
+				return "RPG Maker", "2000"
 
 			match = re.search(br'\x00Software\\(KADOKAWA|Enterbrain)\\RPG2003\x00', file_data)
 			if(not found and match is not None):
 				found = True
 				print("Found RPG Maker version: 2003")
-				set_clip_v("RPG Maker", "2003")
+				return "RPG Maker", "2003"
 
 			# Cocos2d (and Cocos2d-x)
 			if not found:
@@ -158,63 +156,64 @@ def main():
 				for pattern, data in product(patterns, datas):
 						match = pattern.search(data)
 						if match:
-							set_clip_v("Cocos2d", match.group(1))
-							found = True
-							break
+							return "Cocos2d", match.group(1)
 
 			# RGSS3 Player (UNICODE)
 			match = re.search(br'\x52\x00\x47\x00\x53\x00\x53\x00\x33\x00\x20\x00\x50\x00\x6C\x00\x61\x00\x79\x00\x65\x00\x72\x00', file_data)
 			if(not found and match is not None):
 				found = True
 				print("Found RPG Maker version: VX Ace")
-				set_clip_v("RPG Maker", "VX Ace")
+				return "RPG Maker", "VX Ace"
 
 			# Software\Enterbrain\RGSS3\RTP (UNICODE)
 			match = re.search(br'\x53\x00\x6F\x00\x66\x00\x74\x00\x77\x00\x61\x00\x72\x00\x65\x00\x5C\x00\x45\x00\x6E\x00\x74\x00\x65\x00\x72\x00\x62\x00\x72\x00\x61\x00\x69\x00\x6E\x00\x5C\x00\x52\x00\x47\x00\x53\x00\x53\x00\x33\x00\x5C\x00\x52\x00\x54\x00\x50\x00', file_data)
 			if(not found and match is not None):
 				found = True
 				print("Found RPG Maker version: VX Ace")
-				set_clip_v("RPG Maker", "VX Ace")
+				return "RPG Maker", "VX Ace"
 
 			# RGSS2 Player (UNICODE)
 			match = re.search(br'\x52\x00\x47\x00\x53\x00\x53\x00\x32\x00\x20\x00\x50\x00\x6C\x00\x61\x00\x79\x00\x65\x00\x72\x00', file_data)
 			if(not found and match is not None):
 				found = True
 				print("Found RPG Maker version: VX")
-				set_clip_v("RPG Maker", "VX")
+				return "RPG Maker", "VX"
 
 			#RGSS Player (UNICODE)
 			match = re.search(br'\x52\x00\x47\x00\x53\x00\x53\x00\x20\x00\x50\x00\x6C\x00\x61\x00\x79\x00\x65\x00\x72\x00', file_data)
 			if(not found and match is not None):
 				found = True
 				print("Found RPG Maker version: XP")
-				set_clip_v("RPG Maker", "XP")
+				return "RPG Maker", "XP"
 
 			match = re.search(br'd:\\slave\\win32_nw12\\node-webkit\\src\\content\\nw\\src\\shell_main\.cc', file_data)
 			if(not found and match is not None):
 				found = True
 				print("Found RPG Maker version: MV")
-				set_clip_v("RPG Maker", "MV")
+				return "RPG Maker", "MV"
 
 			match = re.search(br'ShiVa 3D Standalone Engine ([^\x20]+)', file_data)
 			if(not found and match is not None):
 				found = True
 				print("Found ShiVa Engine version: " + match.group(1).decode('utf-8'))
-				set_clip_v("ShiVa Engine", match.group(1).decode('utf-8'))
+				return "ShiVa Engine", match.group(1).decode('utf-8')
 
 			match = re.search(br'\x00WOLF_FileReadText', file_data)
 			if(not found and match is not None):
 				found = True
 				print("Found WOLF RPG Editor")
-				set_clip_wolf()
-				set_clip_v("WOLF RPG Editor")
+				return "WOLF RPG Editor", None
 
 			if(found is False):
 				print("Version not found !!!")
-				set_clip_v("!!!")
-				time.sleep(2)
+				return "!!!", None
 			file.close()
  
+def set_clip_detect():
+	set_clip("QUIT PREMATURELY");
+	name, version = detect(sys.argv[1])
+	set_clip_v(name, version)
+
 def set_clip(text):
 	r = Tk()
 	r.withdraw()
@@ -237,4 +236,4 @@ def set_clip_v(name, version=None):
 	set_clip(out_string)
 
 if __name__ == "__main__":
-	main()
+	set_clip_detect()
