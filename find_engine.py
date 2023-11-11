ad12357fd0cfd71908398800ef28194d8d75d5cd
Splendide Imaginarius <119545140+Splendide-Imaginarius@users.noreply.github.com>
2023-11-11 05:39:14 +0000

    Remove superfluous parentheses from if statements
    They are not Pythonic style.

--- find_engine.py
+++ find_engine.py
@@ -54,13 +54,13 @@ def detect(exe):
     exe_path = exe.parent
 
     path = exe_path / "Adobe AIR"
-    if(not found and path.exists()):
+    if not found and path.exists():
         # Probably Adobe AIR (not tested)
         print('Found Adobe AIR')
         return "Adobe AIR", None
 
     path = exe_path / "renpy"
-    if(not found and path.exists()):
+    if not found and path.exists():
         # Probably renpy project
         # Get version from files...
         init_file = path / "__init__.pyo"
@@ -71,17 +71,17 @@ def detect(exe):
         vc_version_exists = vc_version_file.exists() or vc_version_src_file.exists()
         match1 = None
         match2 = None
-        if(init_exists and vc_version_exists):
+        if init_exists and vc_version_exists:
             if init_file.exists():
                 with open(init_file, 'rb') as file:
                     file_data = file.read()
                     match1 = re.search(re.compile(br'vc_versioni\x00{4}((?:i.{4}){3,5})s', re.DOTALL), file_data)
-                    if(match1):
+                    if match1:
                         match1 = struct.unpack("<" + ("cI" * (len(match1.group(1))//5)), match1.group(1))
                         match1 = ".".join(map(str, match1[1::2]))
             if match1 is None:
                 # Parsing init binary failed, fall back to parsing init source
-                if(init_src_file.exists()):
+                if init_src_file.exists():
                     with open(init_src_file, 'r') as file:
                         file_data = file.read()
                         match1_candidates = re.finditer(re.compile(r'version_tuple = \(([0-9]+, [0-9]+, [0-9]+), vc_version\)', re.DOTALL), file_data)
@@ -103,18 +103,18 @@ def detect(exe):
                 with open(vc_version_file, 'rb') as file:
                     file_data = file.read()
                     match2 = re.search(re.compile(br'\x00{3}(i.{4})', re.DOTALL), file_data)
-                    if(match2):
+                    if match2:
                         match2 = struct.unpack("<cI", match2.group(1))
                         match2 = ".".join(map(str, match2[1::2]))
             if match2 is None:
                 # Parsing vc_version binary failed, fall back to parsing vc_version source
-                if(vc_version_src_file.exists()):
+                if vc_version_src_file.exists():
                     with open(vc_version_src_file, 'r') as file:
                         file_data = file.read()
                         match2 = re.search(re.compile(r'vc_version = ([0-9]+)', re.DOTALL), file_data)
-                        if(match2):
+                        if match2:
                             match2 = match2.group(1)
-        if(match1 is not None and match2 is not None):
+        if match1 is not None and match2 is not None:
             found = True
             ver = ".".join([match1, match2])
             print("Found Ren'Py version: " + ver)
@@ -124,15 +124,15 @@ def detect(exe):
             print(match1 is not None)
             print(match2 is not None)
 
-    if(not found):
+    if not found:
         with open(exe, 'rb') as file:
             file_data = file.read()
 
             match = re.search(br'\x00UnityPlayer\/([^\x20]+)', file_data)
 
-            if(match is None):
+            if match is None:
                 player_file = exe_path / "UnityPlayer.dll"
-                if(player_file.exists()):
+                if player_file.exists():
                     with(open(player_file, 'rb')) as file2:
                         file_data2 = file2.read()
                         match = re.search(br'\x00UnityPlayer\/([^\x20]+)', file_data2)
@@ -143,38 +143,38 @@ def detect(exe):
                     file_data2 = open(ggm_files[0], 'rb').read()
                     match = re.search(br'\x00(\d{1,4}\.\d+\.\d+[a-z]+\d+)\x00', file_data2)
 
-            if(not found and match is not None):
+            if not found and match is not None:
                 found = True
                 print("Found Unity version: " + match.group(1).decode('utf-8'))
                 return "Unity", match.group(1).decode('utf-8')
 
             match = re.search(br'Made with GameMaker: Studio\x00', file_data)
-            if(not found and match is not None):
+            if not found and match is not None:
                 found = True
                 print("Found GameMaker")
                 return "GameMaker", None
             else: # Fallback, Windows only
                 match = re.search(br'name="YoYoGames.GameMaker.Runner"', file_data)
-                if(not found and match is not None):
+                if not found and match is not None:
                     found = True
                     print("Found GameMaker")
                     return "GameMaker", None
 
             match = re.search(br'Unreal Engine 3 Licensee\x00{4}Unreal Engine 3', file_data)
-            if(not found and match is not None):
+            if not found and match is not None:
                 found = True
                 print("Found Unreal Engine version: 3")
                 return "Unreal Engine 3", None
 
             # \UE4PrereqSetup (UNICODE)
             match = re.search(br'\x5C\x00\x55\x00\x45\x00\x34\x00\x50\x00\x72\x00\x65\x00\x72\x00\x65\x00\x71\x00\x53\x00\x65\x00\x74\x00\x75\x00\x70\x00', file_data)
-            if(not found and match is not None):
+            if not found and match is not None:
                 found = True
                 print("Found Unreal Engine version: 4")
                 return "Unreal Engine 4", None
 
             match = re.search(br'org\/lwjgl\/LWJGL', file_data)
-            if(not found and match is not None):
+            if not found and match is not None:
                 found = True
                 print("Found LWJGL")
                 return "LWJGL", None
@@ -182,13 +182,13 @@ def detect(exe):
             match = re.search(br'com\/badlogic\/gdx', file_data)
             # NW.js 29 includes this false positive string
             decoy_match = re.search(br"Heavily inspired by LibGDX's CanvasGraphicsRenderer:", file_data)
-            if(not found and match is not None and decoy_match is None):
+            if not found and match is not None and decoy_match is None:
                 found = True
                 print("Found LibGDX")
                 return "LibGDX", None
 
             match = re.search(br'\r\nKirikiri Z Project Contributors\r\nW.Dee, casper', file_data)
-            if(not found and match is not None):
+            if not found and match is not None:
                 found = True
                 print("Found KiriKiri Z")
                 return "KiriKiri Z", None
@@ -197,12 +197,12 @@ def detect(exe):
             if match is None:
                 # This string shows up in the Falcon-mkxp and mkxp-z forks.
                 match = re.search(br'\x00_mkxp_kernel_caller_alias\x00', file_data)
-            if(not found and match is not None):
+            if not found and match is not None:
                 # Try to detect which RGSS version is being used by mkxp.
                 # First we try mkxp.conf, which takes highest priority.
                 # TODO: also try mkxp.json, which is used by the mkxp-z fork.
                 mkxp_conf_path = exe_path / "mkxp.conf"
-                if(mkxp_conf_path.exists()):
+                if mkxp_conf_path.exists():
                     with open(mkxp_conf_path) as mkxp_conf_file:
                         mkxp_conf = configparser.ConfigParser()
                         try:
@@ -254,17 +254,17 @@ def detect(exe):
 
                 # If Game.ini didn't list a Scripts path, then fallback to detecting the archive file directly.
                 scripts_path = exe_path / "Data" / "Scripts.rxdata"
-                if(scripts_path.exists()):
+                if scripts_path.exists():
                     found = True
                     print("Found mkxp version: XP")
                     return "mkxp", "XP"
                 scripts_path = exe_path / "Data" / "Scripts.rvdata"
-                if(scripts_path.exists()):
+                if scripts_path.exists():
                     found = True
                     print("Found mkxp version: VX")
                     return "mkxp", "VX"
                 scripts_path = exe_path / "Data" / "Scripts.rvdata2"
-                if(scripts_path.exists()):
+                if scripts_path.exists():
                     found = True
                     print("Found mkxp version: VX Ace")
                     return "mkxp", "VX Ace"
@@ -275,13 +275,13 @@ def detect(exe):
                 return "mkxp", None
 
             match = re.search(br'\x00Software\\(KADOKAWA|Enterbrain)\\RPG2000\x00', file_data)
-            if(not found and match is not None):
+            if not found and match is not None:
                 found = True
                 print("Found RPG Maker version: 2000")
                 return "RPG Maker", "2000"
 
             match = re.search(br'\x00Software\\(KADOKAWA|Enterbrain)\\RPG2003\x00', file_data)
-            if(not found and match is not None):
+            if not found and match is not None:
                 found = True
                 print("Found RPG Maker version: 2003")
                 return "RPG Maker", "2003"
@@ -306,28 +306,28 @@ def detect(exe):
 
             # RGSS3 Player (UNICODE)
             match = re.search(br'\x52\x00\x47\x00\x53\x00\x53\x00\x33\x00\x20\x00\x50\x00\x6C\x00\x61\x00\x79\x00\x65\x00\x72\x00', file_data)
-            if(not found and match is not None):
+            if not found and match is not None:
                 found = True
                 print("Found RPG Maker version: VX Ace")
                 return "RPG Maker", "VX Ace"
 
             # Software\Enterbrain\RGSS3\RTP (UNICODE)
             match = re.search(br'\x53\x00\x6F\x00\x66\x00\x74\x00\x77\x00\x61\x00\x72\x00\x65\x00\x5C\x00\x45\x00\x6E\x00\x74\x00\x65\x00\x72\x00\x62\x00\x72\x00\x61\x00\x69\x00\x6E\x00\x5C\x00\x52\x00\x47\x00\x53\x00\x53\x00\x33\x00\x5C\x00\x52\x00\x54\x00\x50\x00', file_data)
-            if(not found and match is not None):
+            if not found and match is not None:
                 found = True
                 print("Found RPG Maker version: VX Ace")
                 return "RPG Maker", "VX Ace"
 
             # RGSS2 Player (UNICODE)
             match = re.search(br'\x52\x00\x47\x00\x53\x00\x53\x00\x32\x00\x20\x00\x50\x00\x6C\x00\x61\x00\x79\x00\x65\x00\x72\x00', file_data)
-            if(not found and match is not None):
+            if not found and match is not None:
                 found = True
                 print("Found RPG Maker version: VX")
                 return "RPG Maker", "VX"
 
             #RGSS Player (UNICODE)
             match = re.search(br'\x52\x00\x47\x00\x53\x00\x53\x00\x20\x00\x50\x00\x6C\x00\x61\x00\x79\x00\x65\x00\x72\x00', file_data)
-            if(not found and match is not None):
+            if not found and match is not None:
                 found = True
                 print("Found RPG Maker version: XP")
                 return "RPG Maker", "XP"
@@ -337,7 +337,7 @@ def detect(exe):
             if match is None:
                 # This string shows up in NW.js used by RPG Maker MZ
                 match = re.search(br'nw\.exe\.pdb', file_data)
-            if(not found and match is not None):
+            if not found and match is not None:
                 # NW.js; might or might not be RPG Maker
                 js_files = exe_path.rglob('*.js')
                 match_name = None
@@ -368,18 +368,18 @@ def detect(exe):
                     return "NW.js", None
 
             match = re.search(br'ShiVa 3D Standalone Engine ([^\x20]+)', file_data)
-            if(not found and match is not None):
+            if not found and match is not None:
                 found = True
                 print("Found ShiVa Engine version: " + match.group(1).decode('utf-8'))
                 return "ShiVa Engine", match.group(1).decode('utf-8')
 
             match = re.search(br'\x00WOLF_FileReadText', file_data)
-            if(not found and match is not None):
+            if not found and match is not None:
                 found = True
                 print("Found WOLF RPG Editor")
                 return "WOLF RPG Editor", None
 
-            if(found is False):
+            if found is False:
                 print("Version not found !!!")
                 return None, None
  
@@ -401,10 +401,10 @@ def set_clip(text):
 def set_clip_v(name, version=None):
     build_major_string = ""
     build_full_string = ""
-    if(version is not None):
+    if version is not None:
         m = re.search(r'^([^\.]+)', version)
         build_major_string = "|name=" + name + " " + m.group(0)
-        if(m.group(0) != version):
+        if m.group(0) != version:
             build_full_string = "|build=" + version
     out_string = "{{Infobox game/row/engine|" + name + build_major_string \
     + "|ref=<ref name=\"engineversion\">{{Refcheck|user=" + username + "|date=" \
